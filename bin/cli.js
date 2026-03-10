3bd0e98b3fc4a69c6147b4613dcf7fd71bdceece
jisan25 <shihabunjisan@gmail.com>
2026-03-10 21:08:48 +0600

    Add support for creating submodules and update usage instructions; bump version to 1.1.1

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
-import { createModule } from "../lib/generator.js";
+import { createModule, createSubModule } from "../lib/generator.js";
 
 const args = process.argv.slice(2);
 
 if (args[0] !== "make" || !args[1]) {
-  console.log("Usage: vue-modular make <module-name>");
+  console.log("Usage:");
+  console.log("  vue-modular make <module-name>");
+  console.log("  vue-modular make <parent-module>:<submodule-name>");
   process.exit(1);
 }
 
-createModule(args[1]);
+const input = args[1];
+
+// Check if user wants a submodule
+if (input.includes(":")) {
+  const [parent, child] = input.split(":");
+  createSubModule(parent, child);
+} else {
+  createModule(input);
+}
